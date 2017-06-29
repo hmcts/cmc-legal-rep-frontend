b4c9c8261a8092ceba3b424af585ea1655382e3a
Robert Parkinson <robertparkinson@gmail.com>
2017-06-29 13:21:59 +0100

    updated to use cmc-common-frontend

--- src/main/app/modules/nunjucks/index.ts
+++ src/main/app/modules/nunjucks/index.ts
@@ -28,6 +28,7 @@ export default class Nunjucks {
     app.set('view engine', 'njk')
     const nunjucksEnv = nunjucks.configure([
       path.join(__dirname, '..', '..', 'views'),
+      path.join(__dirname, '..', '..', '..', 'public', 'macros'),
       path.join(__dirname, '..', '..', 'features')
     ], {
       autoescape: true,
