596d74fba7dfe8b132f9a79dbf6bfd565e4bc324
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-07-20 16:45:21 +0100

    written test for your reference

---
+++ src/test/features/claim/routes/your-reference.ts
@@ -0,0 +1,71 @@
+import { expect } from 'chai'
+import * as request from 'supertest'
+import * as config from 'config'
+import * as mock from 'nock'
+
+import '../../../../../../../hmcts/cmc-legal-rep-frontend/src/test/routes/expectations'
+import { checkAuthorizationGuards } from '../../../../../../../hmcts/cmc-legal-rep-frontend/src/test/routes/authorization-check'
+
+import { Paths as ClaimPaths } from 'claim/paths'
+
+import { app } from '../../../../main/app'
+
+import * as idamServiceMock from '../../../http-mocks/idam'
+import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
+
+const cookieName: string = config.get<string>('session.cookieName')
+
+describe('Claim issue: your reference page', () => {
+  beforeEach(() => {
+    mock.cleanAll()
+    draftStoreServiceMock.resolveRetrieve('claim')
+  })
+
+  describe('on GET', () => {
+    checkAuthorizationGuards(app, 'get', ClaimPaths.yourReferencePage.uri, new RegExp('http://localhost:8000/login\\?continue-url=http://127.0.0.1:[0-9]{1,5}/claim/receiver'))
+
+    it('should render page when everything is fine', async () => {
+      idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'claimant')
+
+      await request(app)
+        .get(ClaimPaths.yourReferencePage.uri)
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.successful.withText('Your reference for this claim'))
+    })
+  })
+
+  describe('on POST', () => {
+    checkAuthorizationGuards(app, 'post', ClaimPaths.yourReferencePage.uri, new RegExp('http://localhost:8000/login\\?continue-url=http://127.0.0.1:[0-9]{1,5}/claim/receiver'))
+
+    it('should render page when form is invalid and everything is fine', async () => {
+      idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'claimant')
+
+      await request(app)
+        .post(ClaimPaths.yourReferencePage.uri)
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.successful.withText('Your reference for this claim', 'div class="error-summary"'))
+    })
+
+    it.skip('should return 500 and render error page when form is valid and cannot save draft', async () => {
+      idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'claimant')
+      draftStoreServiceMock.rejectSave('claim', 'HTTP error')
+
+      await request(app)
+        .post(ClaimPaths.yourReferencePage.uri)
+        .set('Cookie', `${cookieName}=ABC`)
+        .send({ yourReference: '1234567890123456789012345' })
+        .expect(res => expect(res).to.be.serverError.withText('Error'))
+    })
+
+    it('should redirect to personal injury when form is valid and everything is fine', async () => {
+      idamServiceMock.resolveRetrieveUserFor(1, 'cmc-private-beta', 'claimant')
+      draftStoreServiceMock.resolveSave('claim')
+
+      await request(app)
+        .post(ClaimPaths.yourReferencePage.uri)
+        .set('Cookie', `${cookieName}=ABC`)
+        .send({ yourReference: '1234567890123456789012345' })
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.personalInjuryPage.uri))
+    })
+  })
+})
