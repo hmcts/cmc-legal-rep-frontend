4f09342c81cb97c5c19280653317dfc98de3f857
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-07-21 09:31:19 +0100

    reworked on preferred court page

--- src/main/features/claim/paths.ts
+++ src/main/features/claim/paths.ts
@@ -7,6 +7,7 @@ export class Paths {
   static readonly personalInjuryPage = new RoutablePath('/claim/personal-injury')
   static readonly yourReferencePage = new RoutablePath('/claim/your-reference')
   static readonly summariseTheClaimPage = new RoutablePath('/claim/summarise-the-claim')
+  static readonly preferredCourtPage = new RoutablePath('/claim/preferred-court')
 }
 
 export class ErrorPaths {
--- src/main/features/claim/routes/preferred-court.ts
+++ src/main/features/claim/routes/preferred-court.ts
@@ -1,30 +1,27 @@
 import * as express from 'express'
 import { Form } from 'app/forms/form'
+import { Paths } from 'claim/paths'
+
 import { FormValidator } from 'app/forms/validation/formValidator'
 import PreferredCourt from 'app/forms/models/preferredCourt'
 import { ClaimDraftMiddleware } from 'claim/draft/claimDraftMiddleware'
 
-class Paths {
-  static readonly preferredCourtRoute: string = '/claim/preferred-court'
-  static readonly preferredCourtView: string = 'claim/preferred-court'
-}
-
 function renderView (form: Form<PreferredCourt>, res: express.Response): void {
-  res.render( Paths.preferredCourtView, { form: form } )
+  res.render(Paths.preferredCourtPage.associatedView, { form: form })
 }
 
 export default express.Router()
-  .get( Paths.preferredCourtRoute, (req: express.Request, res: express.Response) => {
-    renderView( new Form( res.locals.user.claimDraft.preferredCourt ), res )
-  } )
-  .post( Paths.preferredCourtRoute, FormValidator.requestHandler( PreferredCourt, PreferredCourt.fromObject ), (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  .get(Paths.preferredCourtPage.uri, (req: express.Request, res: express.Response) => {
+    renderView(new Form(res.locals.user.claimDraft.preferredCourt), res)
+  })
+  .post(Paths.preferredCourtPage.uri, FormValidator.requestHandler(PreferredCourt, PreferredCourt.fromObject), (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const form: Form<PreferredCourt> = req.body
     if (form.hasErrors()) {
-      renderView( form, res )
+      renderView(form, res)
     } else {
       res.locals.user.claimDraft.preferredCourt = form.model
-      ClaimDraftMiddleware.save( res, next )
-        .then( () => res.redirect( Paths.preferredCourtRoute ) )
+      ClaimDraftMiddleware.save(res, next)
+        .then(() => res.redirect(Paths.preferredCourtPage.uri))
     }
 
-  } )
+  })
