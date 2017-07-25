488600462048a317ea9a84d824eac0c0f5b1ee1e
dharmendra kumar <k_dharmendra@hotmail.com>
2017-07-25 15:05:24 +0100

    ROC-1776: add claimant address (#21)

--- src/main/features/claim/paths.ts
+++ src/main/features/claim/paths.ts
@@ -7,10 +7,12 @@ export class Paths {
   static readonly personalInjuryPage = new RoutablePath('/claim/personal-injury')
   static readonly yourReferencePage = new RoutablePath('/claim/your-reference')
   static readonly summariseTheClaimPage = new RoutablePath('/claim/summarise-the-claim')
+  static readonly claimantAddressPage = new RoutablePath('/claim/claimant-address')
   static readonly preferredCourtPage = new RoutablePath('/claim/preferred-court')
   static readonly representativeNamePage = new RoutablePath('/claim/representative-name')
   static readonly representativeContactsPage = new RoutablePath('/claim/representative-contacts')
   static readonly representativeAddressPage = new RoutablePath('/claim/representative-address')
+  static readonly defendantTypePage = new RoutablePath('/claim/defendant-type')
 }
 
 export class ErrorPaths {
---
+++ src/main/features/claim/routes/claimant-address.ts
@@ -0,0 +1,32 @@
+import * as express from 'express'
+
+import { Paths } from 'claim/paths'
+
+import { Form } from 'forms/form'
+import { FormValidator } from 'forms/validation/formValidator'
+import { Address } from 'forms/models/address'
+
+import { ClaimDraftMiddleware } from 'claim/draft/claimDraftMiddleware'
+
+function renderView (form: Form<Address>, res: express.Response): void {
+  res.render(Paths.claimantAddressPage.associatedView, {
+    form: form,
+    name: res.locals.user.claimDraft.claimant.name.text
+  })
+}
+
+export default express.Router()
+  .get(Paths.claimantAddressPage.uri, (req: express.Request, res: express.Response) => {
+    renderView(new Form(res.locals.user.claimDraft.claimant.address), res)
+  })
+  .post(Paths.claimantAddressPage.uri, FormValidator.requestHandler(Address), (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const form: Form<Address> = req.body
+
+    if (form.hasErrors()) {
+      renderView(form, res)
+    } else {
+      res.locals.user.claimDraft.claimant.address = form.model
+      ClaimDraftMiddleware.save(res, next)
+        .then(() => res.redirect(Paths.defendantTypePage.uri))
+    }
+  })
