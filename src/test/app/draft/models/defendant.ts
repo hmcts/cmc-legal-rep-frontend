66eaf7f1d006c95e8db1d45e9649758877e4bf20
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-07-26 16:54:38 +0100

    Updated mock for draft store

--- src/test/app/draft/models/defendant.ts
+++ src/test/app/draft/models/defendant.ts
@@ -2,12 +2,14 @@ import { expect } from 'chai'
 
 import Defendant from 'drafts/models/defendant'
 import { Address } from 'forms/models/address'
+import Representative from 'drafts/models/representative'
 
 describe( 'Defendant', () => {
   describe( 'constructor', () => {
     it( 'should have instance fields initialised', () => {
       let defendant = new Defendant()
       expect( defendant.address ).to.be.instanceof( Address )
+      expect( defendant.representative ).to.be.instanceof( Representative )
     } )
   } )
 
