ecc5e11df6dc2a982db16e3dd3a9f05dad516e53
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-07-26 14:06:54 +0100

    add rep address

--- src/main/app/drafts/models/defendant.ts
+++ src/main/app/drafts/models/defendant.ts
@@ -1,11 +1,14 @@
 import { Address } from 'forms/models/address'
+import Representative from 'drafts/models/representative'
 
 export default class Defendant {
   address?: Address = new Address()
+  representative: Representative = new Representative()
 
   deserialize (input: any): Defendant {
     if (input) {
       this.address = new Address().deserialize( input.address )
+      this.representative = new Representative().deserialize( input.representative )
     }
     return this
   }
