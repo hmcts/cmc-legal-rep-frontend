00ee1713dd48f51bb8d8069162159b70de98aa85
Surjeet Singh <surjeet.singh@hmcts.net>
2017-07-27 14:47:42 +0100

    adding environment port

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as https from 'https'
 
-const port: number = 4000
+const port: number = process.env.PORT || 4000
 
 if (app.locals.ENV === 'development') {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
