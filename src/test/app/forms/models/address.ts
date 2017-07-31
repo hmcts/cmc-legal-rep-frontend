42eb7c9cc946edb1809942d0492bf74440d3497e
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-07-31 15:54:20 +0100

    actioned sign off comments

--- src/test/app/forms/models/address.ts
+++ src/test/app/forms/models/address.ts
@@ -65,34 +65,74 @@ describe('Address', () => {
       const errors = validator.validateSync(new Address(_.repeat('*', 101), '', '', 'SA1'))
 
       expect(errors.length).to.equal(1)
-      expectValidationError(errors, ValidationErrors.FIRST_LINE_TOO_LONG.replace('$constraint1', '100'))
+      expectValidationError(errors, ValidationErrors.CONTENT_TOO_LONG)
     })
 
     it('should reject address with second line longer then upper limit', () => {
       const errors = validator.validateSync(new Address('Apartment 99', _.repeat('*', 101), '', 'SA1'))
 
       expect(errors.length).to.equal(1)
-      expectValidationError(errors, ValidationErrors.SECOND_LINE_TOO_LONG.replace('$constraint1', '100'))
+      expectValidationError(errors, ValidationErrors.CONTENT_TOO_LONG)
     })
 
     it('should reject address with city longer then upper limit', () => {
       const errors = validator.validateSync(new Address('Apartment 99', '', _.repeat('*', 61), 'SA1'))
 
       expect(errors.length).to.equal(1)
-      expectValidationError(errors, ValidationErrors.CITY_NOT_VALID.replace('$constraint1', '60'))
+      expectValidationError(errors, ValidationErrors.CONTENT_TOO_LONG)
     })
 
     it('should reject address with postcode longer then upper limit', () => {
       const errors = validator.validateSync(new Address('Apartment 99', '', '', _.repeat('*', 9)))
 
       expect(errors.length).to.equal(1)
-      expectValidationError(errors, ValidationErrors.POSTCODE_NOT_VALID.replace('$constraint1', '8'))
+      expectValidationError(errors, ValidationErrors.CONTENT_TOO_LONG)
     })
 
     it('should accept valid address', () => {
       const errors = validator.validateSync(new Address('Apartment 99', '', '', 'SA1'))
 
       expect(errors.length).to.equal(0)
+    })
+  })
+
+  describe('fromObject', () => {
+
+    it('should have address elements undefined when input is undefined', () => {
+      const address = Address.fromObject(undefined)
+
+      expect(address.line1).to.equal(undefined)
+      expect(address.line2).to.equal(undefined)
+      expect(address.city).to.equal(undefined)
+      expect(address.postcode).to.equal(undefined)
+    })
+
+    it('should have address elements undefined when input has undefined element value', () => {
+      const address = Address.fromObject({
+        line1: undefined,
+        line2: undefined,
+        city: undefined,
+        postcode: undefined
+      })
+
+      expect(address.line1).to.equal(undefined)
+      expect(address.line2).to.equal(undefined)
+      expect(address.city).to.equal(undefined)
+      expect(address.postcode).to.equal(undefined)
+    })
+
+    it('should have valid address details elements', () => {
+      const address = Address.fromObject({
+        line1: 'line1',
+        line2: 'line2',
+        city: 'city',
+        postcode: 'postcode'
+      })
+
+      expect(address.line1).to.equal('LINE1')
+      expect(address.line2).to.equal('LINE2')
+      expect(address.city).to.equal('CITY')
+      expect(address.postcode).to.equal('POSTCODE')
     })
   })
 })
