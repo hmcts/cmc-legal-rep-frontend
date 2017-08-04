f9ed7198ba399f653ace7d8c699abc38aae3e8c3
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-08-04 15:15:52 +0100

    Add issue fee page

---
+++ src/main/features/claim/routes/claim-total.ts
@@ -0,0 +1,21 @@
+import * as express from 'express'
+import { Paths } from 'claim/paths'
+import { ClaimDraftMiddleware } from '../draft/claimDraftMiddleware'
+import ErrorHandling from 'common/errorHandling'
+
+export default express.Router()
+
+  .get(Paths.claimTotalPage.uri, (req: express.Request, res: express.Response) => {
+    res.render(Paths.claimTotalPage.associatedView, {
+      feeAmount: 123,
+      higherValue: 1000
+    })
+  })
+
+  .post(Paths.claimTotalPage.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+
+      await ClaimDraftMiddleware.save(res, next)
+      res.redirect('/not-implemented-yet')
+
+    }))
