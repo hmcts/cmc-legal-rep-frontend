f9bf4c62a7687aaeba3bcf2eb4dffde85a9b9fff
Robert Parkinson <robertparkinson@gmail.com>
2017-08-04 11:50:52 +0100

    updated to correctly display errors above inputs

--- src/main/app/forms/models/amount.ts
+++ src/main/app/forms/models/amount.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash'
-import { IsDefined, Max, Min, ValidateIf } from 'class-validator'
+import { IsDefined, Max, Min, ValidateIf, Equals } from 'class-validator'
 import { Fractions } from 'forms/validation/validators/fractions'
 import { Serializable } from 'app/models/serializable'
 
@@ -7,19 +7,20 @@ export class ValidationErrors {
   static readonly UPPER_VALUE_REQUIRED: string = 'Enter a value or choose ‘I can’t state the value’'
   static readonly UPPER_VALUE_AMOUNT_NOT_VALID: string = 'Enter valid upper value'
   static readonly LOWER_VALUE_AMOUNT_NOT_VALID: string = 'Enter valid lower value'
+  static readonly PICK_ONLY_ONE_OPTION: string = 'You have stated an upper value'
   static readonly AMOUNT_INVALID_DECIMALS: string = 'Enter a maximum two decimal places'
 }
 
 export class Amount implements Serializable<Amount> {
   static readonly CANNOT_STATE_VALUE = 'cannot'
 
+  @Equals(Amount.CANNOT_STATE_VALUE, { message: ValidationErrors.PICK_ONLY_ONE_OPTION})
   cannotState?: string
 
   @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
   @Fractions(0, 2, { message: ValidationErrors.AMOUNT_INVALID_DECIMALS })
   lowerValue?: number
 
-  @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
   @IsDefined({ message: ValidationErrors.UPPER_VALUE_REQUIRED })
   @Min(0.01, { message: ValidationErrors.UPPER_VALUE_AMOUNT_NOT_VALID })
   @Max(99999.99, { message: ValidationErrors.UPPER_VALUE_AMOUNT_NOT_VALID })
