059b4c7fb86b9c7302a5c447c08ef4e50428618c
Robert Parkinson <robertparkinson@gmail.com>
2017-08-04 11:52:14 +0100

    put back in validate if

--- src/main/app/forms/models/amount.ts
+++ src/main/app/forms/models/amount.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash'
-import { IsDefined, Max, Min, ValidateIf, Equals } from 'class-validator'
+import { IsDefined, Max, Min, ValidateIf, Contains } from 'class-validator'
 import { Fractions } from 'forms/validation/validators/fractions'
 import { Serializable } from 'app/models/serializable'
 
@@ -14,13 +14,15 @@ export class ValidationErrors {
 export class Amount implements Serializable<Amount> {
   static readonly CANNOT_STATE_VALUE = 'cannot'
 
-  @Equals(Amount.CANNOT_STATE_VALUE, { message: ValidationErrors.PICK_ONLY_ONE_OPTION})
+  @ValidateIf(o => o.upperValue !== undefined)
+  @Contains(Amount.CANNOT_STATE_VALUE, { message: ValidationErrors.PICK_ONLY_ONE_OPTION})
   cannotState?: string
 
   @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
   @Fractions(0, 2, { message: ValidationErrors.AMOUNT_INVALID_DECIMALS })
   lowerValue?: number
 
+  @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
   @IsDefined({ message: ValidationErrors.UPPER_VALUE_REQUIRED })
   @Min(0.01, { message: ValidationErrors.UPPER_VALUE_AMOUNT_NOT_VALID })
   @Max(99999.99, { message: ValidationErrors.UPPER_VALUE_AMOUNT_NOT_VALID })
