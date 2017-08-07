8fd4c9cfd2478be4ea22394a3c101cdebe76abc6
sabahirfan <sabah.irfan@gmail.com>
2017-08-07 15:16:24 +0100

    ROC-1794: pay by fee account (#58)

---
+++ src/main/app/forms/models/FeeAccount.ts
@@ -0,0 +1,36 @@
+import { Serializable } from 'models/serializable'
+import { IsFeeAccount } from 'app/forms/validation/validators/isFeeAccount'
+import { IsDefined } from 'class-validator'
+import { IsNotBlank } from 'forms/validation/validators/isBlank'
+
+export class ValidationErrors {
+  static readonly FEE_ACCOUNT_INVALID: string = 'Enter a valid Fee Account number'
+  static readonly FEE_ACCOUNT_REQUIRED: string = 'Enter your Fee Account number'
+}
+
+export class FeeAccount implements Serializable<FeeAccount> {
+
+  @IsDefined({ message: ValidationErrors.FEE_ACCOUNT_REQUIRED })
+  @IsNotBlank({ message: ValidationErrors.FEE_ACCOUNT_REQUIRED })
+  @IsFeeAccount({ message: ValidationErrors.FEE_ACCOUNT_INVALID })
+  reference?: string
+
+  constructor (reference?: string) {
+    this.reference = reference
+  }
+
+  static fromObject (value?: any): FeeAccount {
+    if (value == null) {
+      return value
+    }
+    return new FeeAccount(value.reference)
+  }
+
+  deserialize (input?: any): FeeAccount {
+    if (input) {
+      this.reference = input.reference
+    }
+
+    return this
+  }
+}
