ae5834544b9556b40d2aab9d6402ce335dec5865
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-08-17 13:34:10 +0100

    Add statement of value

--- src/main/features/claim/routes/details-summary.ts
+++ src/main/features/claim/routes/details-summary.ts
@@ -4,6 +4,7 @@ import FeesClient from 'fees/feesClient'
 import ErrorHandling from 'common/errorHandling'
 import { Amount } from 'forms/models/amount'
 import { PartyTypes } from 'app/forms/models/partyTypes'
+import { YesNo } from 'forms/models/yesNo'
 
 function renderView (res: express.Response, next: express.NextFunction) {
   const claimAmount: Amount = res.locals.user.legalClaimDraft.amount
@@ -11,10 +12,15 @@ function renderView (res: express.Response, next: express.NextFunction) {
     .then((feeAmount: number) => {
       const claimant = res.locals.user.legalClaimDraft.claimant
       const isClaimantIndividual = claimant.claimantDetails.type.value === PartyTypes.INDIVIDUAL.value
+      const isHousingDisrepair = res.locals.user.legalClaimDraft.housingDisrepair.housingDisrepair.value === YesNo.YES.value
+      const isPersonalInjury = res.locals.user.legalClaimDraft.personalInjury.personalInjury.value === YesNo.YES.value
+
       res.render(Paths.detailsSummaryPage.associatedView, {
         legalClaimDraft: res.locals.user.legalClaimDraft,
         feeAmount: feeAmount,
-        isClaimantIndividual: isClaimantIndividual
+        isClaimantIndividual: isClaimantIndividual,
+        isHousingDisrepair: isHousingDisrepair,
+        isPersonalInjury: isPersonalInjury
       })
     })
     .catch(next)
