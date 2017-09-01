3aeab320bc5db3f4cdc77a20960817f038c9372d
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-09-01 15:10:46 +0100

    change id in tests

--- src/main/app/auth/ownershipChecks.ts
+++ src/main/app/auth/ownershipChecks.ts
@@ -4,7 +4,7 @@ import Claim from 'app/claims/models/claim'
 export default class OwnershipChecks {
 
   static checkClaimOwner (user: User, claim: Claim) {
-    if (user.id !== claim.claimantId) {
+    if (user.id !== claim.submitterId) {
       throw new Error('You are not allowed to access this resource')
     }
   }
