cd0d784dbec0baf3f7a2610c3ff9813d2d79137e
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-09-05 15:25:05 +0100

    ROC-2135: Make higher value erred out when cannot selected (#125)
    * Handle error for higher value and cannot state selection
    
    * handled Nan values

--- src/main/features/claim/routes/amount.ts
+++ src/main/features/claim/routes/amount.ts
@@ -1,17 +1,25 @@
 import * as express from 'express'
 import { Paths } from 'claim/paths'
 
-import { Form } from 'forms/form'
-import { Amount } from 'app/forms/models/amount'
+import { Form, FormValidationError } from 'forms/form'
+import { Amount, ValidationErrors } from 'app/forms/models/amount'
 import { FormValidator } from 'forms/validation/formValidator'
 
 import { ClaimDraftMiddleware } from 'claim/draft/claimDraftMiddleware'
 import ErrorHandling from 'common/errorHandling'
+import { ValidationError } from 'class-validator'
 
 function renderView (form: Form<Amount>, res: express.Response): void {
   res.render(Paths.claimAmountPage.associatedView, {
     form: form
   })
+}
+
+function addErrorMessage (form: Form<Amount>, fieldName: string, errorMessage: string): void {
+  const validationError = new ValidationError()
+  validationError.property = fieldName
+  validationError.constraints = { [fieldName]: errorMessage }
+  form.errors.push(new FormValidationError(validationError, ''))
 }
 
 export default express.Router()
@@ -21,6 +29,10 @@ export default express.Router()
   .post(Paths.claimAmountPage.uri, FormValidator.requestHandler(Amount, Amount.fromObject),
     ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
       const form: Form<Amount> = req.body
+
+      if ((form.model.higherValue != null || isNaN(form.model.higherValue)) && form.model.cannotState === Amount.CANNOT_STATE_VALUE) {
+        addErrorMessage(form, 'higherValue', ValidationErrors.VALID_SELECTION_REQUIRED)
+      }
 
       if (form.hasErrors()) {
         renderView(form, res)
