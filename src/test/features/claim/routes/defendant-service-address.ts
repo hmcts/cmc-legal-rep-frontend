8597ae29fde1e2900def3a22f0f2a924ec039786
Robert Parkinson <robertparkinson@users.noreply.github.com>
2017-09-14 10:41:07 +0100

    Feature/ROC-1777 Enter different service address (#135)
    * Add in service address journey
    
    * update failing route tests
    
    * update how address is printed
    
    * Moved toUpperCase to StringUtils class and remvoed console.log
    
    * remove toUpperString fucntion
    
    * update common-frontend version
    
    * removed console.log
    
    * remove toString method
    
    * add unit test for toString method

--- src/test/features/claim/routes/defendant-service-address.ts
+++ src/test/features/claim/routes/defendant-service-address.ts
@@ -14,36 +14,35 @@ import * as idamServiceMock from '../../../http-mocks/idam'
 import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
 
 const cookieName: string = config.get<string>('session.cookieName')
-const pageText = 'Has the defendant got a legal representative'
 const roles: string[] = ['solicitor']
 
-describe('Claim issue: is defendant represented page', () => {
+describe("Claim issue: Defendant's service address page", () => {
   beforeEach(() => {
     mock.cleanAll()
     draftStoreServiceMock.resolveRetrieve('legalClaim')
   })
 
   describe('on GET', () => {
-    checkAuthorizationGuards(app, 'get', ClaimPaths.defendantRepresentedPage.uri)
+    checkAuthorizationGuards(app, 'get', ClaimPaths.defendantServiceAddressPage.uri)
 
     it('should render page when everything is fine', async () => {
       idamServiceMock.resolveRetrieveUserFor(1, ...roles)
       await request(app)
-        .get(ClaimPaths.defendantRepresentedPage.uri)
+        .get(ClaimPaths.defendantServiceAddressPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText(pageText))
+        .expect(res => expect(res).to.be.successful.withText('Address for service'))
     })
   })
 
   describe('on POST', () => {
-    checkAuthorizationGuards(app, 'post', ClaimPaths.defendantRepresentedPage.uri)
+    checkAuthorizationGuards(app, 'post', ClaimPaths.defendantServiceAddressPage.uri)
 
-    it('should render page when form is invalid and everything is fine', async () => {
+    it('should render page when form is invalid', async () => {
       idamServiceMock.resolveRetrieveUserFor(1, ...roles)
       await request(app)
-        .post(ClaimPaths.defendantRepresentedPage.uri)
+        .post(ClaimPaths.defendantServiceAddressPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.successful.withText(pageText, 'div class="error-summary"'))
+        .expect(res => expect(res).to.be.successful.withText('Address for service', 'div class="error-summary"'))
     })
 
     it('should return 500 and render error page when form is valid and cannot save draft', async () => {
@@ -51,41 +50,32 @@ describe('Claim issue: is defendant represented page', () => {
       draftStoreServiceMock.rejectSave('legalClaim', 'HTTP error')
 
       await request(app)
-        .post(ClaimPaths.defendantRepresentedPage.uri)
+        .post(ClaimPaths.defendantServiceAddressPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .send({
-          isDefendantRepresented: 'NO',
-          organisationName: 'organisationName'
-        })
+        .send({ defendantsAddress: 'NO', line1: 'Apt 99', line2: '', city: 'London', postcode: 'E1' })
         .expect(res => expect(res).to.be.serverError.withText('Error'))
     })
 
-    it('should redirect to defendant rep address page when form is valid and user has selected yes', async () => {
+    it('should redirect to defendant addition page when user selects to use the defendants address option', async () => {
       idamServiceMock.resolveRetrieveUserFor(1, ...roles)
       draftStoreServiceMock.resolveSave('legalClaim')
 
       await request(app)
-        .post(ClaimPaths.defendantRepresentedPage.uri)
+        .post(ClaimPaths.defendantServiceAddressPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .send({
-          isDefendantRepresented: 'YES',
-          organisationName: 'organisationName'
-        })
-        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantRepAddressPage.uri))
+        .send({ defendantsAddress: 'YES' })
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantAdditionPage.uri))
     })
 
-    it('should redirect to defendant add page when form is valid and user has selected no', async () => {
+    it('should redirect to defendant addition page when form is valid and address filled in', async () => {
       idamServiceMock.resolveRetrieveUserFor(1, ...roles)
       draftStoreServiceMock.resolveSave('legalClaim')
 
       await request(app)
-        .post(ClaimPaths.defendantRepresentedPage.uri)
+        .post(ClaimPaths.defendantServiceAddressPage.uri)
         .set('Cookie', `${cookieName}=ABC`)
-        .send({
-          isDefendantRepresented: 'NO',
-          organisationName: undefined
-        })
-        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantServiceAddressPage.uri))
+        .send({ defendantsAddress: 'NO', line1: 'Apt 99', line2: '', city: 'London', postcode: 'E1' })
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantAdditionPage.uri))
     })
   })
 })
