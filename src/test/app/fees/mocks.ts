9d48dfba32c02a86168e74028c0c08287a75c96c
vivekr.dr <vivek.reddy@transformuk.com>
2017-09-15 13:01:35 +0100

    * removed failing route tests * used different sonar scanner

--- src/test/app/fees/mocks.ts
+++
@@ -1,8 +0,0 @@
-import * as mock from 'mock-require'
-
-mock('fees/feesClient', {
-  'default': {
-    calculateIssueFee: (amount) => Promise.resolve(100),
-    calculateMaxIssueFee: () => Promise.resolve(1000000)
-  }
-})
