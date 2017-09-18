c1892c2af26ebeb697b95e042c6850ebf8ffb451
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-09-18 13:53:02 +0100

    Restructure amount validation (#137)

--- src/main/app/forms/models/amount.ts
+++ src/main/app/forms/models/amount.ts
@@ -1,8 +1,13 @@
 import * as _ from 'lodash'
-import { IsDefined, IsEmpty, IsNumber, IsOptional, Max, Min, ValidateIf } from 'class-validator'
+import { IsEmpty, IsNumber, IsOptional, Min, ValidateIf } from 'class-validator'
 import { Fractions } from 'forms/validation/validators/fractions'
 import { Serializable } from 'app/models/serializable'
 import { IsLowerThan } from 'app/forms/validation/validators/isLowerThan'
+import { IsSelectedAlong } from 'app/forms/validation/validators/isSelectedAlong'
+import { MinAmountRange } from 'app/forms/validation/validators/minAmountRange'
+import { MaxRangeAmount } from 'app/forms/validation/validators/maxAmountRange'
+import { IsAmountRangeNumber } from 'app/forms/validation/validators/isAmountRangeNumber'
+import { IsAmountRangeDefined } from 'app/forms/validation/validators/isAmountRangeDefined'
 
 export class ValidationErrors {
   static readonly CANNOT_STATE_VALID_SELECTION_REQUIRED: string = 'Choose ‘I can’t state the value’ or enter a higher value'
@@ -16,25 +21,25 @@ export class ValidationErrors {
 export class Amount implements Serializable<Amount> {
   static readonly CANNOT_STATE_VALUE = 'cannot'
   static readonly MAX_ALLOWED = 9999999.99
+  static readonly MIN_ALLOWED = 0.01
 
-  @ValidateIf(o => (o.higherValue && o.higherValue > 0 ) || (o.lowerValue && o.lowerValue > 0))
+  @ValidateIf(o => (o.higherValue != null) || (o.lowerValue != null))
   @IsEmpty({ message: ValidationErrors.CANNOT_STATE_VALID_SELECTION_REQUIRED })
   cannotState?: string
 
-  @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
   @IsOptional()
   @IsNumber({ message: ValidationErrors.LOWER_VALUE_AMOUNT_NOT_VALID })
   @Fractions(0, 2, { message: ValidationErrors.AMOUNT_INVALID_DECIMALS })
-  @Min(0.01, { message: ValidationErrors.LOWER_VALUE_AMOUNT_NOT_VALID })
+  @Min(Amount.MIN_ALLOWED, { message: ValidationErrors.LOWER_VALUE_AMOUNT_NOT_VALID })
   @IsLowerThan('higherValue', { message: ValidationErrors.LOWER_VALUE_LESS_THAN_UPPER_NOT_VALID })
   lowerValue?: number
 
-  @ValidateIf(o => o.cannotState !== Amount.CANNOT_STATE_VALUE)
-  @IsDefined({ message: ValidationErrors.VALID_SELECTION_REQUIRED })
-  @IsNumber({ message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
+  @IsAmountRangeDefined('cannotState', { message: ValidationErrors.VALID_SELECTION_REQUIRED })
+  @IsAmountRangeNumber('cannotState', { message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
   @Fractions(0, 2, { message: ValidationErrors.AMOUNT_INVALID_DECIMALS })
-  @Min(0.01, { message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
-  @Max(Amount.MAX_ALLOWED, { message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
+  @MinAmountRange(Amount.MIN_ALLOWED, 'cannotState', { message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
+  @MaxRangeAmount(Amount.MAX_ALLOWED, 'cannotState', { message: ValidationErrors.HIGHER_VALUE_AMOUNT_NOT_VALID })
+  @IsSelectedAlong('cannotState', 'lowerValue', { message: ValidationErrors.VALID_SELECTION_REQUIRED })
   higherValue?: number
 
   constructor (lowerValue?: any, higherValue?: any, cannotState?: string) {
---
+++ src/main/app/forms/validation/validators/isAmountRangeNumber.ts
@@ -0,0 +1,36 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface
+} from 'class-validator'
+
+@ValidatorConstraint({ name: 'isAmountRangeNumber' })
+export class IsAmountRangeNumberConstraint implements ValidatorConstraintInterface {
+
+  validate (value: any, args: ValidationArguments) {
+    const cannotStateInput = (args.object as any)[args.constraints[0]]
+
+    if (cannotStateInput === 'cannot' && (value === null || value === undefined)) {
+      return true
+    }
+
+    return typeof value === 'number'
+  }
+}
+
+/**
+ * Validator validates only amount range value for number when cannot state is not selected.
+ */
+export function IsAmountRangeNumber (cannotState: string, validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [cannotState],
+      validator: IsAmountRangeNumberConstraint
+    })
+  }
+}
