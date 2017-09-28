4fabd7ebd465f1583449fb782326c36b8e272bcc
sabahirfan <sabah.irfan@gmail.com>
2017-09-28 10:58:46 +0100

    Add missing route tests (#164)

---
+++ src/test/features/claim/routes/defendant-change.ts
@@ -0,0 +1,121 @@
+import { expect } from 'chai'
+import * as request from 'supertest'
+import * as config from 'config'
+import * as mock from 'nock'
+
+import '../../../routes/expectations'
+import { checkAuthorizationGuards } from './checks/authorization-check'
+
+import { Paths as ClaimPaths } from 'claim/paths'
+
+import { app } from '../../../../main/app'
+
+import * as idamServiceMock from '../../../http-mocks/idam'
+import * as draftStoreServiceMock from '../../../http-mocks/draft-store'
+
+const cookieName: string = config.get<string>('session.cookieName')
+const roles: string[] = ['solicitor']
+
+const defendants = {
+  defendants: [{
+    address: {
+      line1: 'Apt 99',
+      city: 'London',
+      postcode: 'E1'
+    },
+    representative: {
+      organisationName: 'Defendant Organisation Name',
+      address: {
+        line1: 'Apt 99',
+        line2: 'Building A',
+        city: 'London',
+        postcode: 'E1'
+      }
+    },
+    defendantDetails: {
+      type: 'INDIVIDUAL',
+      title: 'title',
+      fullName: 'fullName'
+    }
+  },
+    {
+      address: {
+        line1: 'Apt 99',
+        city: 'London',
+        postcode: 'E1'
+      },
+      representative: {
+        organisationName: 'Defendant Organisation Name',
+        address: {
+          line1: 'Apt 99',
+          line2: 'Building A',
+          city: 'London',
+          postcode: 'E1'
+        }
+      },
+      defendantDetails: {
+        type: 'INDIVIDUAL',
+        title: 'title',
+        fullName: 'fullName'
+      }
+    }]
+}
+
+describe('Claim issue: Defendant change page', () => {
+  beforeEach(() => {
+    mock.cleanAll()
+    draftStoreServiceMock.resolveSave('legalClaim')
+    draftStoreServiceMock.resolveRetrieve('view')
+    draftStoreServiceMock.resolveSave('view')
+    idamServiceMock.resolveRetrieveUserFor(1, ...roles)
+  })
+
+  describe('on GET', () => {
+    checkAuthorizationGuards(app, 'get', ClaimPaths.defendantChangePage.uri)
+
+    it('should redirect to defendant type page for one existing defendant when everything is fine', async () => {
+      draftStoreServiceMock.resolveRetrieve('legalClaim')
+
+      await request(app)
+        .get(ClaimPaths.defendantChangePage.uri + '?index=1')
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantTypePage.uri))
+    })
+
+    it('should redirect to defendant address page when everything is fine', async () => {
+      draftStoreServiceMock.resolveRetrieve('legalClaim', defendants)
+
+      await request(app)
+        .get(ClaimPaths.defendantChangePage.uri + '?index=1&page=address')
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantAddressPage.uri))
+    })
+
+    it('should redirect to defendant represented page when everything is fine', async () => {
+      draftStoreServiceMock.resolveRetrieve('legalClaim', defendants)
+
+      await request(app)
+        .get(ClaimPaths.defendantChangePage.uri + '?index=1&page=represented')
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantRepresentedPage.uri))
+    })
+
+    it('should redirect to defendant represented address page when everything is fine', async () => {
+      draftStoreServiceMock.resolveRetrieve('legalClaim', defendants)
+
+      await request(app)
+        .get(ClaimPaths.defendantChangePage.uri + '?index=1&page=reps-address')
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantRepAddressPage.uri))
+    })
+
+    it('should redirect to defendant service address page when everything is fine', async () => {
+      draftStoreServiceMock.resolveRetrieve('legalClaim', defendants)
+
+      await request(app)
+        .get(ClaimPaths.defendantChangePage.uri + '?index=1&page=service-address')
+        .set('Cookie', `${cookieName}=ABC`)
+        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.defendantServiceAddressPage.uri))
+    })
+  })
+})
