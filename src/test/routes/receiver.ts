090cd9ba0658e32fb2409e5489ed321169019965
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-10-06 10:03:16 +0100

    Feature/toggle fix for default (#174)

--- src/test/routes/receiver.ts
+++ src/test/routes/receiver.ts
@@ -11,7 +11,7 @@ import { app } from '../../main/app'
 import * as idamServiceMock from '../http-mocks/idam'
 
 const cookieName: string = config.get<string>('session.cookieName')
-const roles: string[] = ['solicitor']
+const token = 'I am dummy access token'
 
 describe('Claim issue: post login receiver', () => {
   beforeEach(() => {
@@ -21,23 +21,16 @@ describe('Claim issue: post login receiver', () => {
   describe('on GET', () => {
     describe('for authorized user', () => {
 
-      it('should save JWT token in cookie when JWT token exists in query string', async () => {
+      it('should save JWT token in cookie', async () => {
+        idamServiceMock.resolveRetrieveAuthTokenFor(token)
 
         await request(app)
-          .get(`${AppPaths.receiver.uri}?jwt=ABC`)
-          .expect(res => expect(res).to.have.cookie(cookieName, 'ABC'))
-      })
-
-      it('should redirect to start page for jwt when everything is fine', async () => {
-        idamServiceMock.resolveRetrieveUserFor(1, ...roles)
-
-        await request(app)
-          .get(`${AppPaths.receiver.uri}?jwt=ABC`)
-          .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.startPage.uri))
+          .get(`${AppPaths.receiver.uri}?code=ABC`)
+          .expect(res => expect(res).to.have.cookie(cookieName, token))
       })
 
       it('should redirect to start page for authToken when everything is fine', async () => {
-        idamServiceMock.resolveRetrieveAuthTokenFor()
+        idamServiceMock.resolveRetrieveAuthTokenFor(token)
 
         await request(app)
           .get(`${AppPaths.receiver.uri}?code=code`)
