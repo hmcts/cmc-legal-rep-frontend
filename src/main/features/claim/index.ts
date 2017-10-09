6ea7d5cbe7d6b4f4af0b9f1c45071454bb50b181
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-10-09 15:57:56 +0100

    Refactor idam call (#177)

--- src/main/features/claim/index.ts
+++ src/main/features/claim/index.ts
@@ -2,7 +2,6 @@ import * as express from 'express'
 import * as config from 'config'
 import * as path from 'path'
 import { Paths as AppPaths } from 'app/paths'
-import * as uuid from 'uuid'
 import * as toBoolean from 'to-boolean'
 
 import { AuthorizationMiddleware } from 'idam/authorizationMiddleware'
@@ -10,18 +9,16 @@ import { ClaimDraftMiddleware } from 'claim/draft/claimDraftMiddleware'
 import { RouterFinder } from 'common/router/routerFinder'
 import { buildURL } from 'utils/callbackBuilder'
 import { ViewDraftMiddleware } from 'views/draft/viewDraftMiddleware'
+import { OAuthHelper } from 'idam/oAuthHelper'
 
 function claimIssueRequestHandler (): express.RequestHandler {
   function accessDeniedCallback (req: express.Request, res: express.Response): void {
-    const clientId = config.get<string>('oauth.clientId')
     const redirectUri = buildURL(req, AppPaths.receiver.uri.substring(1))
-    const state = uuid()
-
-    const idamUrlWithOauth = `${config.get('idam.authentication-web.url')}/login?response_type=code&state=${state}&client_id=${clientId}&redirect_uri=${redirectUri}`
-    const idamUrlWithoutOauth = `${config.get('idam.authentication-web.url')}/login?continue-url=${redirectUri}`
-
     const useOauth = toBoolean(config.get<boolean>('featureToggles.idamOauth'))
-    res.redirect(useOauth ? idamUrlWithOauth : idamUrlWithoutOauth)
+
+    res.redirect(useOauth
+      ? OAuthHelper.getRedirectUri(req, res)
+      : `${config.get('idam.authentication-web.url')}/login?continue-url=${redirectUri}`)
   }
 
   const requiredRoles = ['solicitor']
--- src/main/routes/receiver.ts
+++ src/main/routes/receiver.ts
@@ -1,24 +1,13 @@
 import * as express from 'express'
 import * as config from 'config'
 import { Paths as ClaimPaths } from 'claim/paths'
-import { buildURL } from 'utils/callbackBuilder'
-import { Paths as AppPaths } from 'app/paths'
 import * as Cookies from 'cookies'
-import IdamClient from 'idam/idamClient'
 import { AuthToken } from 'idam/authToken'
 import * as toBoolean from 'to-boolean'
-import { AuthTokenRequest } from 'idam/authTokenRequest'
-
-async function getAuthToken (req: express.Request) {
-  const { code } = req.query
-  const redirectUri = buildURL(req, AppPaths.receiver.uri.substring(1))
-  const clientId = config.get<string>('oauth.clientId')
-  const clientSecret = config.get<string>('oauth.clientSecret')
-  const auth = `Basic ${new Buffer(clientId + ':' + clientSecret).toString('base64')}`
-  const body = new AuthTokenRequest('authorization_code', code, redirectUri)
-
-  return await IdamClient.createAuthToken(`${config.get('idam.api.url')}/oauth2/token`, auth, body)
-}
+import { OAuthHelper } from 'idam/oAuthHelper'
+import { Paths as AppPaths } from 'app/paths'
+import IdamClient from 'idam/idamClient'
+import { buildURL } from 'utils/callbackBuilder'
 
 export default express.Router()
   .get('/receiver', async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
@@ -27,10 +16,15 @@ export default express.Router()
     const cookies = new Cookies(req, res)
     const useOauth = toBoolean(config.get<boolean>('featureToggles.idamOauth'))
 
-    if (req.query.jwt) {
+    if (!useOauth && req.query.jwt) {
       cookies.set(sessionCookie, req.query.jwt, { sameSite: 'lax' })
-    } else if (useOauth) {
-      const authToken: AuthToken = await getAuthToken(req)
+    } else if (useOauth && req.query.code) {
+      if (req.query.state !== OAuthHelper.getStateCookie(req)) {
+        throw new Error('Invalid state')
+      }
+
+      res.clearCookie('state')
+      const authToken: AuthToken = await IdamClient.exchangeCode(req.query.code, buildURL(req, AppPaths.receiver.uri.substring(1)))
       cookies.set(sessionCookie, authToken.accessToken, { sameSite: 'lax' })
     }
 
