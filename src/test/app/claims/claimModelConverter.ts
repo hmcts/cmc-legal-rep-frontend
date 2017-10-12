70846f1a158d98a2006c5c4efe506e0925cf74bd
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-10-12 13:55:17 +0100

    Fix test

--- src/test/app/claims/claimModelConverter.ts
+++ src/test/app/claims/claimModelConverter.ts
@@ -5,13 +5,9 @@ import { ClaimModelConverter } from 'app/claims/claimModelConverter'
 import { claimDraft as draftTemplate } from '../../data/draft/claimDraft'
 import { claimData as entityTemplate } from '../../data/entity/claimData'
 
-import {
-  claimants, defendants
-} from '../../data/draft/partyDetails'
+import { claimants, defendants } from '../../data/draft/partyDetails'
 
-import {
-  claimants as claimantsEntity, defendants as defendantsEntity
-} from '../../data/entity/party'
+import { claimants as claimantsEntity, defendants as defendantsEntity } from '../../data/entity/party'
 
 import DraftLegalClaim from 'drafts/models/draftLegalClaim'
 import ClaimData from 'claims/models/claimData'
@@ -34,11 +30,26 @@ function prepareClaimData (claimants: object, defendants: object): ClaimData {
 
 describe('ClaimModelConverter', () => {
 
-  it.skip(`should convert claim issued by individual against organisation`, () => {
+  it(`should convert claim issued by individual against organisation`, () => {
     const claimDraft = prepareClaimDraft(claimants, defendants)
-    const claimData = prepareClaimData(claimantsEntity, defendantsEntity)
+    const claimData: ClaimData = prepareClaimData(claimantsEntity, defendantsEntity)
+    const target: any = ClaimModelConverter.convert(claimDraft)
 
-    expect(ClaimModelConverter.convert(claimDraft)).to.deep.equal(claimData)
+    expect(target.externalId).to.equal(claimData.externalId)
+    expect(target.feeAmountInPennies).to.equal(claimData.feeAmountInPennies)
+    expect(target.feeCode).to.equal(claimData.feeCode)
+    expect(target.feeAccountNumber).to.equal(claimData.feeAccountNumber)
+    expect(target.externalReferenceNumber).to.equal(claimData.externalReferenceNumber)
+    expect(target.preferredCourt).to.equal(claimData.preferredCourt)
+    expect(target.reason).to.equal(claimData.reason)
+    expect(target.statementOfTruth.signerName).to.equal(claimData.statementOfTruth.signerName)
+    expect(target.statementOfTruth.signerRole).to.equal(claimData.statementOfTruth.signerRole)
+    expect(target.amount.type).to.equal(claimData.amount.type)
+    expect(target.amount.cannotState).to.equal(claimData.amount.cannotState)
+    expect(target.amount.lowerValue).to.equal(claimData.amount.lowerValue)
+    expect(target.amount.higherValue).to.equal(claimData.amount.higherValue)
+    expect(target.claimants.length).to.equal(claimData.claimants.length)
+    expect(target.defendants.length).to.equal(claimData.defendants.length)
   })
 
 })
