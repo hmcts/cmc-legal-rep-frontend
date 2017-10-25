1aba5c2515973dd15359e629e32b2161b8d129c5
Robert Parkinson <robertparkinson@users.noreply.github.com>
2017-10-25 10:17:14 +0100

    ROC-2486 Legal Rep Details stored in cookie (#181)
    * Store organisation name in cookie
    
    * store and retrieve details using cookie
    
    * update organisationName to match pattern and write test for representativeDetails
    
    * update to use aes128
    
    * update to add sameSite
    
    * remove empty string initilisation

---
+++ src/main/common/cookieProperties.ts
@@ -0,0 +1,20 @@
+export default class CookieProperties {
+
+  static getCookieParameters () {
+    return {
+      signed: true,
+      httpOnly: true,
+      maxAge: 7889238000,
+      secure: true,
+      sameSite: true
+    }
+  }
+
+  static getCookieConfig () {
+    return {
+      options: {
+        algorithm: 'aes128'
+      }
+    }
+  }
+}
