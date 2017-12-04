7f8ff4a79db787ca8bdd34002329812c8ddbf147
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-12-04 11:58:44 +0000

    Retrieve sealed claim from Document Management (#227)

--- src/main/features/claim/routes/receipt.ts
+++ src/main/features/claim/routes/receipt.ts
@@ -8,12 +8,22 @@ import DocumentsClient from 'app/documents/documentsClient'
 import ClaimStoreClient from 'app/claims/claimStoreClient'
 import Claim from 'app/claims/models/claim'
 
+async function getSealedClaimFromDocumentStore (res: express.Response, claim: Claim) {
+  const binaryUrl: string = await DocumentsClient.getBinaryUrl(res.locals.user.bearerToken, claim.sealedClaimDocumentSelfPath)
+  return DocumentsClient.getDocument(res.locals.user.bearerToken, binaryUrl)
+}
+
 export default express.Router()
   .get(Paths.receiptReceiver.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const { externalId } = req.params
     try {
       const claim: Claim = await ClaimStoreClient.retrieveByExternalId(externalId, res.locals.user.id)
-      DocumentsClient.getSealedClaim(externalId, res.locals.user)
+
+      const sealedClaim = claim.sealedClaimDocumentSelfPath === undefined
+        ? DocumentsClient.getSealedClaim(externalId, res.locals.user)
+        : await getSealedClaimFromDocumentStore(res, claim)
+
+      sealedClaim
         .on('response', (response: http.IncomingMessage) => {
           if (response.statusCode !== 200) {
             return next(new Error('Unexpected error during document retrieval'))
