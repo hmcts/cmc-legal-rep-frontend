e39d3b988ce4a76e25bd4c6373559d10519d193d
Robert Parkinson <robertparkinson@gmail.com>
2017-12-08 09:29:09 +0000

    wrap get in error handling method

--- src/main/features/certificateOfService/routes/document-remove.ts
+++ src/main/features/certificateOfService/routes/document-remove.ts
@@ -3,20 +3,22 @@ import { Paths } from 'certificateOfService/paths'
 import { UploadedDocument } from 'claims/models/uploadedDocument'
 import User from 'idam/user'
 import { ForbiddenError } from '../../../errors'
+import ErrorHandling from 'common/errorHandling'
 import { DraftService } from 'services/draftService'
 
 export default express.Router()
-  .get(Paths.documentRemovePage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const user: User = res.locals.user
-    const documentToDelete: UploadedDocument = user.legalCertificateOfServiceDraft.document.uploadedDocuments.find(document => document.documentManagementURI === req.query.id)
-    if (documentToDelete === undefined) {
-      throw new ForbiddenError()
-    } else {
-      const updatedDocumentsList: UploadedDocument[] = user.legalCertificateOfServiceDraft.document.uploadedDocuments.filter(function (document) {
-        return document.documentManagementURI !== documentToDelete.documentManagementURI
-      })
-      user.legalCertificateOfServiceDraft.document.uploadedDocuments = updatedDocumentsList
-      await new DraftService().save(res.locals.user.legalCertificateOfServiceDraft, res.locals.user.bearerToken)
-      res.redirect(Paths.documentUploadPage.uri)
-    }
-  })
+  .get(Paths.documentRemovePage.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const user: User = res.locals.user
+      const documentToDelete: UploadedDocument = user.legalCertificateOfServiceDraft.document.uploadedDocuments.find(document => document.documentManagementURI === req.query.id)
+      if (documentToDelete === undefined) {
+        throw new ForbiddenError()
+      } else {
+        const updatedDocumentsList: UploadedDocument[] = user.legalCertificateOfServiceDraft.document.uploadedDocuments.filter(function (document) {
+          return document.documentManagementURI !== documentToDelete.documentManagementURI
+        })
+        user.legalCertificateOfServiceDraft.document.uploadedDocuments = updatedDocumentsList
+        await new DraftService().save(res.locals.user.legalCertificateOfServiceDraft, res.locals.user.bearerToken)
+        res.redirect(Paths.documentUploadPage.uri)
+      }
+    }))
