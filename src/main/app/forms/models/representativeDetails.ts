8dac1349b273eb8ef8dfeca4ce31968c58c51077
Robert Parkinson <robertparkinson@users.noreply.github.com>
2017-12-19 11:06:24 +0000

    Bug/roc 2828 per user cookie (#239)
    * add yarn.lock file
    
    * update to allow per user cookies on the same machine
    
    * refactor code to pass in request objects rather than the request
    
    * remove extra if statement

--- src/main/app/forms/models/representativeDetails.ts
+++ src/main/app/forms/models/representativeDetails.ts
@@ -2,29 +2,34 @@ import { Address } from 'forms/models/address'
 import { ContactDetails } from 'forms/models/contactDetails'
 import { OrganisationName } from 'forms/models/organisationName'
 import { FeeAccount } from 'forms/models/feeAccount'
-import * as express from 'express'
-import CookieProperties from 'common/cookieProperties'
 
 export class RepresentativeDetails {
-  organisationName: OrganisationName = new OrganisationName()
-  address: Address = new Address()
-  contactDetails: ContactDetails = new ContactDetails()
-  feeAccount: FeeAccount = new FeeAccount()
+  id: string
+  organisationName: OrganisationName
+  address: Address
+  contactDetails: ContactDetails
+  feeAccount: FeeAccount
   cookieName: string
 
-  constructor () {
-    this.organisationName = new OrganisationName()
-    this.address = new Address()
-    this.contactDetails = new ContactDetails()
-    this.feeAccount = new FeeAccount()
+  constructor (id?: string, organisationName?: OrganisationName, address?: Address, contactDetails?: ContactDetails,
+               feeAccount?: FeeAccount) {
+    this.id = id
+    this.organisationName = organisationName
+    this.address = address
+    this.contactDetails = contactDetails
+    this.feeAccount = feeAccount
     this.cookieName = 'legalRepresentativeDetails'
   }
 
-  public static getCookie (req: express.Request): RepresentativeDetails {
-    return req.signedCookies.legalRepresentativeDetails === undefined ? new RepresentativeDetails() : req.signedCookies.legalRepresentativeDetails
-  }
-
-  public static saveCookie (res: express.Response, legalRepDetails: RepresentativeDetails) {
-    res.cookie(legalRepDetails.cookieName, legalRepDetails, CookieProperties.getCookieParameters())
+  deserialize (input: any): RepresentativeDetails {
+    if (input) {
+      this.id = input.id
+      this.organisationName = input.organisationName
+      this.address = input.address
+      this.contactDetails = input.contactDetails
+      this.feeAccount = input.feeAccount
+      this.cookieName = input.cookieName
+    }
+    return this
   }
 }
