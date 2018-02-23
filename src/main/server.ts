b82386c8c5016925a0203e1724e0a0dc6ff3a8fb
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-02-23 13:08:00 +0000

    Add application runner

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,27 +1,15 @@
 #!/usr/bin/env node
 
+import './ts-paths-bootstrap'
+import * as config from 'config'
+import * as toBoolean from 'to-boolean'
+
 import { app } from './app'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as https from 'https'
-import { Logger } from '@hmcts/nodejs-logging'
+import { ApplicationCluster } from './applicationCluster'
+import { ApplicationRunner } from './applicationRunner'
 
-const logger = Logger.getLogger('server')
-
-const port: number = parseInt(process.env.PORT, 10) || 4000
-
-if (app.locals.ENV === 'development' || app.locals.ENV === 'dockertests') {
-  const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl')
-  const serverOptions = {
-    key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
-    cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt'))
-  }
-  const server = https.createServer(serverOptions, app)
-  server.listen(port, () => {
-    logger.info(`Application started: https://localhost:${port}/legal`)
-  })
+if (toBoolean(config.get<boolean>('featureToggles.clusterMode'))) {
+  ApplicationCluster.execute(() => ApplicationRunner.run(app))
 } else {
-  app.listen(port, () => {
-    logger.info(`Application started: http://localhost:${port}/legal`)
-  })
+  ApplicationRunner.run(app)
 }
