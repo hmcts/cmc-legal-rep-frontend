2841edb815a3cefa93159237b2692076044ab893
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-28 08:17:39 +0000

    Remove unused application cluster
    No coverage on it, so lets delete it!

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
 
 import './ts-paths-bootstrap'
-import * as config from 'config'
-import * as toBoolean from 'to-boolean'
 
 import { app } from './app'
-import { ApplicationCluster } from './applicationCluster'
 import { ApplicationRunner } from './applicationRunner'
 
-if (toBoolean(config.get<boolean>('featureToggles.clusterMode'))) {
-  ApplicationCluster.execute(() => ApplicationRunner.run(app))
-} else {
-  ApplicationRunner.run(app)
-}
+ApplicationRunner.run(app)
