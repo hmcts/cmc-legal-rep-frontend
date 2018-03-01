c2b3d3c08ccd42e7126df3c97fa5bb41fbdebb9b
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-01 07:47:27 +0000

    Add application insights

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,6 +2,10 @@
 
 import './ts-paths-bootstrap'
 
+import { AppInsights } from 'modules/app-insights'
+// App Insights needs to be enabled as early as possible as it monitors other libraries as well
+AppInsights.enable()
+
 import { app } from './app'
 import { ApplicationRunner } from './applicationRunner'
 
