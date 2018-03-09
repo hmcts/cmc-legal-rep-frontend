70e83c8940651f1fed3c43398cf9893519b373ce
Sabah Irfan <sabah.irfan@gmail.com>
2018-03-09 14:53:33 +0000

    Simplify mock response

--- src/test/http-mocks/pay.ts
+++ src/test/http-mocks/pay.ts
@@ -6,29 +6,9 @@ const payUrl = config.get<string>('pay.url')
 const payPath = '/' + config.get<string>('pay.path')
 
 const paymentSuccessResponse: object = {
-  status: 'Success',
-  amount: 60,
-  description: 'Money Claim issue fee',
   reference: 'RC-1520-4276-0065-8715',
-  payment_reference: 'RC-1520-4276-0065-8715',
-  currency: 'GBP',
-  ccd_case_number: 'UNKNOWN',
-  case_reference: 'dfd75bac-6d54-4c7e-98f7-50e047d7c7f5',
-  channel: 'online',
-  method: 'card',
-  external_provider: 'gov pay',
-  external_reference: 'solicitor reference',
-  site_id: 'AA00',
-  service_name: 'Civil Money Claims',
-  fees: [
-    {
-      code: 'X0026',
-      version: '1',
-      calculated_amount: 60
-    }],
-  _links: {
-    self: { href: 'http://localhost:4421/card-payments/RC-1520-4276-0065-8715', method: 'GET' }
-  }
+  status: 'Success',
+  date_created: '18-02-2018 17:24:46.477Z'
 }
 
 export function resolveCreate () {
