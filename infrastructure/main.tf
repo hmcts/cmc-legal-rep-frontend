dd0928a7208f06d3eb7d3d409d166e1736b0c0f6
Tim Jacomb <tim.jacomb@hmcts.net>
2018-06-19 07:32:21 +0100

    ROC-3940 Remove hashi vault provider
    Now that the hashi secrets are removed from the state the provider can be removed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,3 @@
-provider "vault" {
-  //  # It is strongly recommended to configure this provider through the
-  //  # environment variables described above, so that each user can have
-  //  # separate credentials set in the environment.
-  //  #
-  //  # This will default to using $VAULT_ADDR
-  //  # But can be set explicitly
-  address = "https://vault.reform.hmcts.net:6200"
-}
-
 data "azurerm_key_vault" "cmc_key_vault" {
   name = "${local.vaultName}"
   resource_group_name = "${local.vaultName}"
