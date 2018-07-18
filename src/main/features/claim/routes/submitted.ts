70c5c19752fefda17305caaebe9fdae48eb168f1
Tim Jacomb <tim.jacomb@hmcts.net>
2018-07-18 09:46:37 +0100

    UI cleanup

--- src/main/features/claim/routes/submitted.ts
+++ src/main/features/claim/routes/submitted.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
-import { Paths } from 'claim/paths'
+import { Paths as ClaimPaths } from 'claim/paths'
+import { Paths as DashboardPaths } from 'dashboard/paths'
 import ErrorHandling from 'shared/errorHandling'
 import Claim from 'claims/models/claim'
 import ClaimStoreClient from 'claims/claimStoreClient'
@@ -9,21 +10,21 @@ async function renderView (req: express.Request, res: express.Response): Promise
 
   const claim: Claim = await ClaimStoreClient.retrieveByExternalId(externalId, res.locals.user)
 
-  res.render(Paths.claimSubmittedPage.associatedView, {
+  res.render(ClaimPaths.claimSubmittedPage.associatedView, {
     claim: claim,
     claimNumber: claim.claimNumber,
     feePaid: claim.claimData.feeAmountInPennies,
     repEmail: claim.submitterEmail,
-    receiptPath: Paths.receiptReceiver.uri.replace(':externalId', externalId)
+    receiptPath: ClaimPaths.receiptReceiver.uri.replace(':externalId', externalId)
   })
 }
 
 export default express.Router()
-  .get(Paths.claimSubmittedPage.uri,
+  .get(ClaimPaths.claimSubmittedPage.uri,
     ErrorHandling.apply(async (req: express.Request, res: express.Response): Promise<void> => {
       await renderView(req, res)
     }))
 
-  .post(Paths.claimSubmittedPage.uri, (req: express.Request, res: express.Response) => {
-    res.redirect(Paths.startPage.uri)
+  .post(ClaimPaths.claimSubmittedPage.uri, (req: express.Request, res: express.Response) => {
+    res.redirect(DashboardPaths.searchPage.uri)
   })
