b2822fe8f8f7f87338bc7005834e4dbbc5481365
Tim Jacomb <t.jacomb@kainos.com>
2018-07-19 16:53:38 +0100

    Remove auto login on register (#355)

--- src/main/app/idam/jwtExtractor.ts
+++ src/main/app/idam/jwtExtractor.ts
@@ -3,15 +3,7 @@ import * as config from 'config'
 export default class JwtExtractor {
 
   static extract (req: any): string {
-    if (!req) {
-      return undefined
-    }
-    if (req.query && req.query.jwt) {
-      return req.query.jwt
-    }
-    if (req.cookies) {
-      return req.cookies[config.get<string>('session.cookieName')]
-    }
+    return req.cookies[config.get<string>('session.cookieName')]
   }
 
 }
--- src/test/app/idam/jwtExtractor.ts
+++ src/test/app/idam/jwtExtractor.ts
@@ -1,23 +1,12 @@
 import * as config from 'config'
 import { expect } from 'chai'
+import { Request } from 'express'
 
 import JwtExtractor from 'idam/jwtExtractor'
 
 const sessionCookieName = config.get<string>('session.cookieName')
 
 describe('Extracting JWT', () => {
-  it('should return token from query string', () => {
-    const jwtValue = 'a'
-
-    const req = {
-      query: {
-        jwt: jwtValue
-      }
-    }
-
-    expect(JwtExtractor.extract(req)).to.equal(jwtValue)
-  })
-
   it('should return token from cookie', () => {
     const jwtValue = 'a'
 
@@ -25,28 +14,8 @@ describe('Extracting JWT', () => {
       cookies: {
         [sessionCookieName]: jwtValue
       }
-    }
+    } as Request
 
     expect(JwtExtractor.extract(req)).to.equal(jwtValue)
-  })
-
-  it('should return token from query string first', () => {
-    const jwtValue = 'a'
-
-    const req = {
-      query: {
-        jwt: jwtValue
-      },
-      cookies: {
-        [sessionCookieName]: 'b'
-      }
-    }
-
-    expect(JwtExtractor.extract(req)).to.equal(jwtValue)
-  })
-
-  it('should return undefined if no cookie', () => {
-    const req = {}
-    expect(JwtExtractor.extract(req)).to.equal(undefined)
   })
 })
