d8972799014bf6d71a925bff270e5716302ce7fc
Sabah Irfan <sabah.irfan@gmail.com>
2019-03-14 17:25:37 +0000

    Add config

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 import './ts-paths-bootstrap'
+import * as config from 'config'
+import * as propertiesVolume from '@hmcts/properties-volume'
 
 import { AppInsights } from 'modules/app-insights'
-// App Insights needs to be enabled as early as possible as it monitors other libraries as well
-AppInsights.enable()
-
 import { app } from './app'
 import { ApplicationRunner } from './applicationRunner'
+
+propertiesVolume.addTo(config)
+
+// App Insights needs to be enabled as early as possible as it monitors other libraries as well
+AppInsights.enable()
 
 ApplicationRunner.run(app)
