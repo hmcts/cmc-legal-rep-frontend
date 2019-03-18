18f850ba87713ad2765813668efb8ea62dd6d67b
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-03-18 10:29:53 +0000

    Update server.ts
    Changing the placement of injecting properties volume

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 import './ts-paths-bootstrap'
+import * as config from 'config'
+import * as propertiesVolume from '@hmcts/properties-volume'
+propertiesVolume.addTo(config)
 
 import { AppInsights } from 'modules/app-insights'
 import { app } from './app'
 import { ApplicationRunner } from './applicationRunner'
-
-require('@hmcts/properties-volume').addTo(require('config'))
 
 // App Insights needs to be enabled as early as possible as it monitors other libraries as well
 AppInsights.enable()
