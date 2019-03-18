fecd798078ec5229aed2421de0296fa0761fb0ca
Sabah Irfan <sabah.irfan@gmail.com>
2019-03-18 09:36:30 +0000

    Use javascript style of importing

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
 import './ts-paths-bootstrap'
-import * as config from 'config'
-import * as propertiesVolume from '@hmcts/properties-volume'
 
 import { AppInsights } from 'modules/app-insights'
 import { app } from './app'
 import { ApplicationRunner } from './applicationRunner'
 
-propertiesVolume.addTo(config)
+require('@hmcts/properties-volume').addTo(require('config'))
 
 // App Insights needs to be enabled as early as possible as it monitors other libraries as well
 AppInsights.enable()
