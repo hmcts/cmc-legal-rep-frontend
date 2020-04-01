80f1224039a0ca348c926f7ab0f52e0e769ae862
Karim Lawani <k7lawani@gmail.com>
2020-04-01 12:00:35 +0100

    remove terraform ase from code base (#697)
    * remove terraform ase from code base
    
    * update output

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -17,40 +17,7 @@ variable "env" { }
 
 variable "ilbIp" { }
 
-variable "payments_api_url" {
-  default = "http://payment-api-saat.service.core-compute-saat.internal"
-}
-
-variable "fees_api_url" {
-  default = "http://fees-register-api-saat.service.core-compute-saat.internal"
-}
-
-variable "idam_api_url" {
-  default = "http://betaDevBccidamAppLB.reform.hmcts.net"
-}
-
-variable "authentication_web_url" {
-  default = "https://idam-test.dev.ccidam.reform.hmcts.net"
-}
-
 variable "subscription" {}
-
-// feature toggles
-variable "feature_dashboard" {
-  default = "true"
-}
-
-variable "feature_certificateOfService" {
-  default = "false"
-}
-
-variable "feature_inversionOfControl" {
-  default = "false"
-}
-
-variable "feature_return_error_to_user" {
-  default = "false"
-}
 
 variable "jenkins_AAD_objectId" {
   type                        = "string"
@@ -59,49 +26,4 @@ variable "jenkins_AAD_objectId" {
 
 variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
-}
-
-variable "client_id" {
-  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
-}
-
-variable "external_host_name" {
-  default = "moneyclaims-legal.sandbox.platform.hmcts.net"
-}
-
-variable "appinsights_instrumentation_key" {
-  description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
-}
-
-variable "ga_tracking_id" {
-  description = "Google Analytics tracking ID"
-  default = "UA-97111056-3"
-}
-
-variable "feedback_legal_service_survey" {
-  description = "End of service survey link"
-  default = "http://www.smartsurvey.co.uk/s/not-a-real-legal-service-survey/"
-}
-
-variable "feedback_legal_report_problem_survey" {
-  description = "Report a problem survey link"
-  default = "http://www.smartsurvey.co.uk/s/not-a-real-legal-service-survey/"
-}
-
-variable "feedback_legal_survey" {
-  description = "Report a problem survey link"
-  default = "http://www.smartsurvey.co.uk/s/not-a-real-legal-service-survey/"
-}
-
-variable "capacity" {
-  default = "1"
-}
-
-variable "common_tags" {
-  type = "map"
-}
-
-variable "enable_ase" {
-  default = false
 }
