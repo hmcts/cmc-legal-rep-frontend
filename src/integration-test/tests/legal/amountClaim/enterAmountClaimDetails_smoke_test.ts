e8f5cba8f9f9ee9f5807f983241776673400ae62
prudhvi-v <prudhvi.veebhuthi@HMCTS.NET>
2020-09-09 12:37:29 +0100

    Updating smoke test with random user rather than depending on environment variables

--- src/integration-test/tests/legal/amountClaim/enterAmountClaimDetails_smoke_test.ts
+++ src/integration-test/tests/legal/amountClaim/enterAmountClaimDetails_smoke_test.ts
@@ -2,8 +2,6 @@ import I = CodeceptJS.I
 
 import { verifyPageData } from 'integration-test/data/legal-test-data'
 
-import { SMOKE_TEST_SOLICITOR_USERNAME, SMOKE_TEST_USER_PASSWORD } from 'integration-test/data/test-data'
-
 import { AmountClaimSteps } from 'integration-test/tests/legal/amountClaim/steps/amountClaims'
 import { UserSteps } from 'integration-test/tests/legal/home/steps/user'
 import { DefendantSteps } from 'integration-test/tests/legal/defence/steps/defendant'
@@ -14,9 +12,15 @@ const defendantSteps: DefendantSteps = new DefendantSteps()
 
 Feature('Enter claim amount and submit claim (smoke)')
 
-Scenario('I can fill both Claimant, Defendant details move up to submit claim @smoke-test', async (I: I) => {
-  userSteps.loginAndStartClaim(SMOKE_TEST_SOLICITOR_USERNAME, SMOKE_TEST_USER_PASSWORD)
+Before(async (I: I) => {
+  const userEmail = await I.createSolicitorUser()
+  userSteps.loginAndStartClaim(userEmail)
   userSteps.enterClaimantServiceDetails()
+})
+Scenario('I can fill both Claimant, Defendant details move up to submit claim @smoke-test', async (I: I) => {
+  // commenting below as there are not variables set yet. Creating a random solicitor for now in "Before"
+  // userSteps.loginAndStartClaim(SMOKE_TEST_SOLICITOR_USERNAME, SMOKE_TEST_USER_PASSWORD)
+  // userSteps.enterClaimantServiceDetails()
   userSteps.enterClaimantTypeOrganisation()
   I.see('Claimant: ' + verifyPageData.claimantOrganization)
   userSteps.enterClaimantAddress()
