44982affbbd05e351634bd433309851226769423
yogendra.upasani <yogendra.upasani@hmcts.net>
2020-10-19 20:01:07 +0100

    ROC-8543 Reference data changes

--- src/main/app/forms/models/OrganisationEntityResponse.ts
+++ src/main/app/forms/models/OrganisationEntityResponse.ts
@@ -1,6 +1,10 @@
 import { OrganisationMinimalInfoResponse } from 'forms/models/organisationMinimalInfoResponse'
+import { Serializable } from 'models/serializable'
 
-export class OrganisationEntityResponse {
+export class OrganisationEntityResponse implements Serializable <OrganisationEntityResponse> {
+  deserialize (obj: any): OrganisationEntityResponse {
+    throw new Error('Method not implemented.')
+  }
   readonly sraId: string
   readonly paymentAccount: Array<string>
   readonly organisationMinimalInfo: OrganisationMinimalInfoResponse
--- src/main/app/forms/models/OrganisationMinimalInfoResponse.ts
+++ src/main/app/forms/models/OrganisationMinimalInfoResponse.ts
@@ -1,4 +1,9 @@
-export class OrganisationMinimalInfoResponse {
+import { Serializable } from 'models/serializable'
+
+export class OrganisationMinimalInfoResponse implements Serializable <OrganisationMinimalInfoResponse> {
+  deserialize (obj: any): OrganisationMinimalInfoResponse {
+    throw new Error('Method not implemented.')
+  }
   readonly name: string
   readonly organisationIdentifier: string
 }
