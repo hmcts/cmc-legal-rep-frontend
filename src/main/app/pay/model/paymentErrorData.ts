076f6827875c544c183da0f3041497fcd7cb3b93
pradhap87 <prathap.mathiyalagan@hmcts.net>
2020-10-21 19:03:49 +0100

    fixing lint error

--- src/main/app/pay/model/paymentErrorData.ts
+++ src/main/app/pay/model/paymentErrorData.ts
@@ -2,10 +2,10 @@ import { Expose } from 'class-transformer'
 import { StatusHistories } from 'pay/model/paymentErrorResponse'
 
 export class ErrorData {
-    @Expose({ name: 'status_histories' })
-    readonly StatusHistories: Array<StatusHistories>
-    readonly date_created: string
-    readonly payment_group_reference: string
-    readonly reference: string
-    readonly status: string
-  }
+  @Expose({ name: 'status_histories' })
+  readonly StatusHistories: Array<StatusHistories>
+  readonly date_created: string
+  readonly payment_group_reference: string
+  readonly reference: string
+  readonly status: string
+}
