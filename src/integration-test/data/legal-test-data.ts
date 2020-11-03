1bff01d5dfaf5de9a9897ad6ee46a5c04d1f1729
Prathap Mathiyalagan <41957682+pradhap87@users.noreply.github.com>
2020-11-03 13:23:08 +0000

    Updated new PBA number (#897)
    * Updated new PBA number
    
    * Updated TEST DATA

--- src/integration-test/data/legal-test-data.ts
+++ src/integration-test/data/legal-test-data.ts
@@ -1,12 +1,12 @@
 export const verifyPageData = {
-  organizationRefNumber: 'PBA1234567',
+  organizationRefNumber: 'PBA0081334',
   claimantName: 'Mr Benugo',
   defendantName: 'Mr Pret',
   claimantOrganization: 'Abc corporation',
   defendantOrganization: 'Def corporation',
   defendantRepOrganization: 'Defendant Rep Ltd',
   emailConfirmation: 'We\'ve emailed confirmation to: ',
-  feeAccountNumber: 'PBA0000000',
+  feeAccountNumber: 'PBA0081334',
   feesPaid: '£455',
   maxFeePaid: '£10,000'
 }
