d6a000aaeb3ffaec3737fc822ed3d72ef5946cee
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-10 10:50:42 +0000

    updated TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,11 @@ locals {
   vaultName = "${var.raw_product}-${var.env}"
 }
 
+data "azurerm_key_vault" "cmc_key_vault" {
+  name = "${local.vaultName}"
+  resource_group_name = "${local.vaultName}"
+}
+
 data "azurerm_key_vault_secret" "app_insights_instrumental_key" {
   name = "AppInsightsInstrumentationKey"
   key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
