81619b9b388b603143ac9ae46f46aa7cdce25bbe
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-10 10:33:09 +0000

    update TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,12 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  vaultName = "${var.raw_product}-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "app_insights_instrumental_key" {
+  name = "AppInsightsInstrumentationKey"
+  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+}
