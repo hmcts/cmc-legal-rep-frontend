1e088929607f6ba0a99dc3afa10dbbe9d0f9d2fd
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-02-10 11:20:33 +0000

    updated TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,11 +7,11 @@ locals {
 }
 
 data "azurerm_key_vault" "cmc_key_vault" {
-  name = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
+  name = local.vaultName
+  resource_group_name = local.vaultName
 }
 
 data "azurerm_key_vault_secret" "app_insights_instrumental_key" {
   name = "AppInsightsInstrumentationKey"
-  key_vault_id = "${data.azurerm_key_vault.cmc_key_vault.id}"
+  key_vault_id = data.azurerm_key_vault.cmc_key_vault.id
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -20,7 +20,7 @@ variable "ilbIp" { }
 variable "subscription" {}
 
 variable "jenkins_AAD_objectId" {
-  type                        = "string"
+  type                        = map(string)
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
