991be8ed3868c1cfca191a606ccd90f78a72ea61
Rob Doherty <robdohertykelly@gmail.com>
2021-02-22 17:11:15 +0000

    TA-383: additional e2e test waiting for benefit of tests running in Safari browser

--- src/integration-test/helpers/pageHelper.js
+++ src/integration-test/helpers/pageHelper.js
@@ -23,22 +23,7 @@ class PageHelper extends Helper {
   async waitForLegalAppPage (path = '', sec) {
     const helper = this.helpers['WebDriver']
     const timeout = sec || helper.options.waitForTimeout
-    const baseUrl = `${legalAppBaseURL}${path}`
-    let currUrl = ''
-
-    return helper
-      .waitUntil(async () => {
-        currUrl = await helper.grabCurrentUrl()
-        return currUrl.startsWith(baseUrl)
-      }, timeout)
-      .catch((e) => {
-        if (e.message.indexOf('timed out')) {
-          const timeoutMessage =`expected url to start with ${baseUrl}, but found ${currUrl}`
-          console.error(`ERROR: ${timeoutMessage}`)   // needed to see the error if called from a Before() block
-          throw new Error(timeoutMessage);
-        }
-        throw e;
-      })
+    return helper.waitForText('Issue civil court proceedings', timeout, 'h1')
   }
 }
 
