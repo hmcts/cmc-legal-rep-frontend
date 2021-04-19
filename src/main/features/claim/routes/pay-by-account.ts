abdfc0a4130bfcad259605f2cf7575d42ca72120
pradhap87 <prathap.mathiyalagan@hmcts.net>
2021-04-19 22:28:13 +0100

    unit test fix

--- src/main/features/claim/routes/pay-by-account.ts
+++ src/main/features/claim/routes/pay-by-account.ts
@@ -21,9 +21,9 @@ import { PayClient } from 'pay/payClient'
 import { PaymentResponse } from 'pay/model/paymentResponse'
 import { ServiceAuthTokenFactoryImpl } from 'shared/security/serviceTokenFactoryImpl'
 import { User } from 'idam/user'
-import { YourReference } from 'forms/models/yourReference'
-import Representative from 'drafts/models/representative'
-import { OrganisationName } from 'forms/models/organisationName'
+// import { YourReference } from 'forms/models/yourReference'
+// import Representative from 'drafts/models/representative'
+// import { OrganisationName } from 'forms/models/organisationName'
 
 const logger = Logger.getLogger('router/pay-by-account')
 
@@ -57,11 +57,7 @@ async function updateHandler (res, next, externalId: string) {
     }
   })
   .catch(err => {
-    if (err.statusCode === HttpStatus.CONFLICT) {
-      deleteDraftAndRedirect(res, next, externalId)
-    } else {
-      next(err)
-    }
+    next(err)
   })
 }
 
@@ -120,15 +116,15 @@ export default express.Router()
 
         draft.document.feeAccount = form.model
         draft.document.ccdCaseId = draft.document.ccdCaseId === undefined ? ccdCaseNumber : draft.document.ccdCaseId
-        if (draft.document.yourReference === undefined) {
-          draft.document.yourReference = new YourReference()
-        }
+        // if (draft.document.yourReference === undefined) {
+        //   draft.document.yourReference = new YourReference()
+        // }
         draft.document.yourReference.reference = draft.document.yourReference.reference === undefined ? yourReference : draft.document.yourReference.reference
-        if (draft.document.representative === undefined) {
-          draft.document.representative = new Representative()
-          draft.document.representative.organisationName = new OrganisationName()
-        }
-        draft.document.representative.organisationName.name = orgName
+        // if (draft.document.representative === undefined) {
+        //   draft.document.representative = new Representative()
+        //   draft.document.representative.organisationName = new OrganisationName()
+        // }
+        // draft.document.representative.organisationName.name = orgName
         pbaNumber = form.model.reference
 
         const feeResponse: FeeResponse = await FeesClient.getFeeAmount(draft.document.amount)
