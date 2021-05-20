f7ec14a30cc2fc0400f9fb005fbffdd1108df86e
Prathap Mathiyalagan <41957682+pradhap87@users.noreply.github.com>
2021-05-20 12:15:24 +0100

    Update idamHelper.ts

--- src/integration-test/helpers/idamHelper.ts
+++ src/integration-test/helpers/idamHelper.ts
@@ -1,13 +1,16 @@
+/* tslint:disable:no-console */
+
 import { IdamClient } from 'integration-test/helpers/clients/idamClient'
 
 class IdamHelper extends codecept_helper {
-  createSolicitorUser (): Promise<string> {
-    return this.createRandomUser('cmc-solicitor')
-  }
-
-  private async createRandomUser (userGroupCode: string): Promise<string> {
-    const email: string = this.generateRandomEmailAddress()
-    await IdamClient.createUser(email, userGroupCode)
+  async createSolicitorUser (): Promise<string> {
+    let email: string
+    try {
+      email = this.generateRandomEmailAddress()
+      await IdamClient.createUser(email)
+    } catch (e) {
+      console.log(`Error creating user: ${e.message}`)
+    }
     return email
   }
 
