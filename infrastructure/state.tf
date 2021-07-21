1319a0b223de2ff64b48f8c0c07d33740fc96298
dilipsamrahmcts <dilip.samra@hmcts.net>
2021-07-21 12:55:52 +0100

    feature/ROC-9307-masterbot-pr upgrading hashicorp/terraform version

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.61.0"
+      version = "=2.68.0"
     }
     random = {
       source = "hashicorp/random"
