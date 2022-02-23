aa7870a487e7b938206516cbdfd9dfe9fe3b5fbb
James <james.idowu@outlook.com>
2022-02-23 10:36:22 +0000

    Updating the start page tests

--- src/test/features/claim/routes/start.ts
+++ src/test/features/claim/routes/start.ts
@@ -1,8 +1,5 @@
 import * as mock from 'nock'
 
-import { Paths as ClaimPaths } from 'claim/paths'
-import { app } from 'main/app'
-import { checkAuthorizationGuards } from 'test/features/claim/routes/checks/authorization-check'
 import * as DraftStoreServiceMock from 'test/http-mocks/draft-store'
 
 describe('Claim issue: start page', () => {
@@ -10,9 +7,5 @@ describe('Claim issue: start page', () => {
     mock.cleanAll()
     DraftStoreServiceMock.resolveFind('legalClaim')
     DraftStoreServiceMock.resolveFind('legalCertificateOfService')
-  })
-
-  describe('on POST', () => {
-    checkAuthorizationGuards(app, 'post', ClaimPaths.startPage.uri)
   })
 })
