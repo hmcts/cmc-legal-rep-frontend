5ab78e17d0fd0362bba29e6e64b1a0c30d15ed9c
James <james.idowu@outlook.com>
2022-02-23 10:21:07 +0000

    Removing the post method to the /claim/start page and updating the tests

--- src/main/features/claim/routes/start.ts
+++ src/main/features/claim/routes/start.ts
@@ -1,30 +1,8 @@
 import * as express from 'express'
 import { Paths } from 'claim/paths'
-import { DraftService } from 'services/draftService'
-import { Draft } from '@hmcts/draft-store-client'
-import { DraftLegalClaim } from 'drafts/models/draftLegalClaim'
-import { DraftCertificateOfService } from 'drafts/models/draftCertificateOfService'
 
 export default express.Router()
 
   .get(Paths.startPage.uri, (req: express.Request, res: express.Response) => {
     res.redirect('https://www.gov.uk/government/publications/myhmcts-how-to-make-a-damages-claim-online/issue-a-claim-for-damages-with-myhmcts') // redirect LRPB issue claim page to issue damages claims page
-  })
-
-  .post(Paths.startPage.uri, async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
-    const draft: Draft<DraftLegalClaim> = res.locals.legalClaimDraft
-    const legalCertificateOfServiceDraft: Draft<DraftCertificateOfService> = res.locals.legalCertificateOfServiceDraft
-    try {
-      if (draft && draft.id) {
-        await new DraftService().delete(draft.id, res.locals.user.bearerToken)
-      }
-
-      if (legalCertificateOfServiceDraft && legalCertificateOfServiceDraft.id) {
-        await new DraftService().delete(legalCertificateOfServiceDraft.id, res.locals.user.bearerToken)
-      }
-      res.redirect(Paths.representativeNamePage.uri)
-    } catch (err) {
-      next(err)
-    }
-
   })
--- src/test/features/claim/routes/start.ts
+++ src/test/features/claim/routes/start.ts
@@ -1,16 +1,9 @@
-import { expect } from 'chai'
-import * as request from 'supertest'
-import * as config from 'config'
 import * as mock from 'nock'
 
 import { Paths as ClaimPaths } from 'claim/paths'
 import { app } from 'main/app'
-import * as idamServiceMock from 'test/http-mocks/idam'
 import { checkAuthorizationGuards } from 'test/features/claim/routes/checks/authorization-check'
 import * as DraftStoreServiceMock from 'test/http-mocks/draft-store'
-
-const cookieName: string = config.get<string>('session.cookieName')
-const roles: string[] = ['solicitor']
 
 describe('Claim issue: start page', () => {
   beforeEach(() => {
@@ -21,29 +14,5 @@ describe('Claim issue: start page', () => {
 
   describe('on POST', () => {
     checkAuthorizationGuards(app, 'post', ClaimPaths.startPage.uri)
-
-    it('should return 500 and render error page when can not delete draft claim', async () => {
-      idamServiceMock.resolveRetrieveUserFor('1', ...roles)
-      DraftStoreServiceMock.rejectDelete(100, 'HTTP error')
-
-      await request(app)
-        .post(ClaimPaths.startPage.uri)
-        .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.serverError.withText('Error'))
-    })
-
-    it('should redirect to representative-name page when delete previous draft is successful', async () => {
-      idamServiceMock.resolveRetrieveUserFor('1', ...roles)
-      DraftStoreServiceMock.resolveDelete()
-      DraftStoreServiceMock.resolveDelete()
-      DraftStoreServiceMock.resolveDelete()
-      DraftStoreServiceMock.resolveDelete()
-      idamServiceMock.resolveRetrieveServiceToken()
-
-      await request(app)
-        .post(ClaimPaths.startPage.uri)
-        .set('Cookie', `${cookieName}=ABC`)
-        .expect(res => expect(res).to.be.redirect.toLocation(ClaimPaths.representativeNamePage.uri))
-    })
   })
 })
